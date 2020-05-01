3755682fd9b2545c9605c110b04530d5d77b57ca
Spark <919205480@qq.com>
2020-05-01 18:08:37 +0800

    1

---
+++ 4.30/src/com/bit/demo/TestDemo.java
@@ -0,0 +1,12 @@
+package com.bit.demo;
+
+import java.util.Arrays;
+import java.util.*;  //导入util下的所有包
+
+public class TestDemo {
+    public static void main(String[] args) {
+        int[] array={1,2,3,4,5,6};
+        System.out.println(Arrays.toString(array));
+        Date date=new Date();
+    }
+}
